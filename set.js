66a26f3e57b42c134691c1c32eaa984adfbbea46
Shogun0811 <167612797+Shogun0811@users.noreply.github.com>
2024-12-09 06:38:02 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,9 +1,9 @@
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
+  PREFIXE: process.env.PREFIXE || "💫",
   NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "221760263631",
+  MODE: process.env.MODE || "private",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_uH9aUDWN_SESSION-ID",
   DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 };
